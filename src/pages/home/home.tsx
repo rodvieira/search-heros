24ae5754104d638071b090a3234db99f74934592
Rodrigo Vieira <rodrigodenizads@gmail.com>
2021-06-20 14:12:05 -0300

    refactor: alterando a requisicao para a rota correta

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -4,7 +4,7 @@ import { AxiosHttpGetClient } from '@/service/http/axios-http-get-client/axios-h
 const Home: React.FC = () => {
   const fetchUsers = async () => {
     const request = new AxiosHttpGetClient()
-    const response = await request.get({ url: 'users' });
+    const response = await request.get({ url: '/characters' })
     
     console.log(response);
   };
