bbd538f6d9fd3ee69a416b3368985b9bf9963941
Rodrigo Vieira <rodrigodenizads@gmail.com>
2021-06-21 11:29:50 -0300

    refactor: refatoração na pagina Home

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,14 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-import { HeaderHome, FiltersContent } from '@/components'
+import { AxiosHttpGetClient } from '@/service/http/axios-http-get-client/axios-http-get-client'
+import { Container, HeaderHome, FiltersContent, FlexContent, CardHero } from '@/components'
 import Styles from './home-styles.scss'
 
 const Home: React.FC = () => {
+
+  const [heros, setHeros] = useState([])
+
+  const fetchCharacters = async () => {
+    const req = new AxiosHttpGetClient();
+    const res = await req.get({ url: '/characters' })
+    const { data } = res
+
+    setHeros(data.data.results)
+  };
+
+  useEffect(() => {
+    fetchCharacters();
+  }, [])
   return (
-    <div className={Styles.home}>
+    <Container>
       <HeaderHome />
       <FiltersContent />
-    </div>
+      <FlexContent>
+        {heros.map(hero => (
+          <CardHero
+            thumbnail={`${hero.thumbnail.path}.${hero.thumbnail.extension}`}
+            name={hero.name}
+          />
+        ))}
+      </FlexContent>
+    </Container>
   )
 }
 
