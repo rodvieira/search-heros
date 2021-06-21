8c7b1a33674f480916f0f022d272a8073b89f2c0
Rodrigo Vieira <rodrigodenizads@gmail.com>
2021-06-21 11:25:27 -0300

    refactor: deixando a logo responsiva

--- src/components/logo/logo.tsx
+++ src/components/logo/logo.tsx
@@ -1,7 +1,8 @@
 import React, { memo } from 'react'
 
 import IconLogo from '@/assets/logo.svg'
+import Styles from './logo-styles.scss'
 
-const Logo: React.FC = () => <img src={IconLogo} />
+const Logo: React.FC = () => <img src={IconLogo} className={Styles.logo} />
 
 export default memo(Logo)
