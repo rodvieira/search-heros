5e9168ac4146fdee823bd55e9c545df471838201
Rodrigo Vieira <rodrigodenizads@gmail.com>
2021-06-22 22:03:37 -0300

    feat: adicionado a pagina do heroi

--- src/pages/hero/hero.tsx
+++ src/pages/hero/hero.tsx
@@ -1,15 +1,53 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom';
 
-import Styles from './hero-styles.scss'
-
-import { HeaderHero, Container, DetailsHero } from '@/components'
+import { HeaderHero, Container, DetailsHero, ComicsHero } from '@/components'
+import { AxiosHttpGetClient } from '@/service/http/axios-http-get-client/axios-http-get-client';
+import { Character } from '@/protocols/character';
+import { Comics } from '@/protocols/comics';
 
 const Hero: React.FC = () => {
+  const { id }: any = useParams();
+
+  const [character, setCharacter] = useState<Character>();
+  const [characterComics, setCharacterComics] = useState<Comics[]>([])
+
+  const fetchCharacter = async () => {
+    const req = new AxiosHttpGetClient();
+    const res = await req.get({ url: `/characters/${id}` })
+    const res2 = await req.get({ url: `/characters/${id}/comics?orderBy=onsaleDate&limit=10` })
+
+    const dataCharacter = res.data.data.results[0]
+    const dataCharacterComics = res2.data.data.results
+
+    setCharacter({
+      name: dataCharacter.name,
+      description: dataCharacter.description,
+      comicsCount: dataCharacter.comics.available,
+      seriesCount: dataCharacter.series.available,
+      thumbnail: dataCharacter.thumbnail
+    })
+
+    const newCharacterComics = dataCharacterComics.map(item => {
+      return {
+        title: item.title,
+        thumbnail: item.thumbnail
+      }
+    })
+
+    setCharacterComics(newCharacterComics)
+  };
+
+  useEffect(() => {
+    fetchCharacter()
+  }, [])
+
   return (
     <div style={{ background: '#e7f6e7'}}>
       <HeaderHero />
       <Container size="1200">
-        <DetailsHero />
+        <DetailsHero character={character} />
+        <ComicsHero characterComics={characterComics} />
       </Container>
     </div>
   )
