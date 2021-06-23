c06a3d8412975f7f3f65cd9d76ec9d3ea258c012
Rodrigo Vieira <rodrigodenizads@gmail.com>
2021-06-23 00:33:29 -0300

    feat: adicionado funções no component de input

--- src/components/input-search/input-search.tsx
+++ src/components/input-search/input-search.tsx
@@ -3,14 +3,20 @@ import Styles from './input-search-styles.scss'
 
 type Props = {
   custom?: Boolean
+  sendQuery?: Function
 }
 
-const InputSearch: React.FC<Props> = ({ custom }: Props) => {
+const InputSearch: React.FC<Props> = ({ custom, sendQuery }: Props) => {
+  const handleKeyDown = (e) => {
+    if (e.key === 'Enter') sendQuery(e.target.value)
+  }
+
   return (
     <input
       type="text"
       placeholder="Procure por heróis"
       className={`${Styles.inputSearch} ${custom && Styles.custom}`}
+      onKeyDown={(e) => handleKeyDown(e)}
     />
   )
 }
