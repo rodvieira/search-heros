9d368c85b9b0cf4f5fcd6738032b5bf90ce928d9
Rodrigo Vieira <rodrigodenizads@gmail.com>
2021-06-23 20:44:47 -0300

    feat: adição de atributos na interface de characters

--- src/protocols/character.ts
+++ src/protocols/character.ts
@@ -1,9 +1,10 @@
 import { Thumbnail } from './thumbnail'
 
 export interface Character {
+  id: number
   name: string
-  description: string
-  comicsCount: number
-  seriesCount: number
+  description?: string
+  comicsCount?: number
+  seriesCount?: number
   thumbnail: Thumbnail
 }
