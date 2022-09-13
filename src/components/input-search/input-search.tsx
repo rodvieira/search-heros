43b0bdc0f4c8aae7f84bf7d2262cb4bb28d30019
Rodrigo Vieira <rodrigodenizads@gmail.com>
2022-09-12 23:43:02 -0300

    refactor: change a types in InputSearch component

--- src/components/input-search/input-search.tsx
+++ src/components/input-search/input-search.tsx
@@ -2,21 +2,22 @@ import React from 'react'
 import Styles from './input-search-styles.scss'
 
 type Props = {
-  custom?: Boolean
-  sendQuery?: Function
+  custom?: boolean
+  sendQuery?: (query: string) => void
 }
 
-const InputSearch: React.FC<Props> = ({ custom, sendQuery }: Props) => {
-  const handleKeyDown = (e) => {
-    if (e.key === 'Enter') sendQuery(e.target.value)
+const InputSearch: React.FC<Props> = ({ custom = false, sendQuery }: Props) => {
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') sendQuery(e.currentTarget.value)
   }
 
   return (
     <input
+      data-testid="input-search"
       type="text"
       placeholder="Procure por heróis"
-      className={`${Styles.inputSearch} ${custom && Styles.custom}`}
-      onKeyDown={(e) => handleKeyDown(e)}
+      className={`${Styles.inputSearch} ${!!custom ? Styles.custom : ''}`}
+      onKeyDown={handleKeyDown}
     />
   )
 }
