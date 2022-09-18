890add7bfff457668b756d7ae8ba8f3cd5a2fbc9
Rodrigo Vieira <rodrigodenizads@gmail.com>
2022-09-18 20:28:33 -0300

    test: test component Container

---
+++ src/components/container/container.spec.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Container } from '@/components'
+import { render, screen } from '@testing-library/react'
+
+const makeSut = () => {
+  return render(
+    <Container size="500">
+      <div>children</div>
+    </Container>
+  )
+}
+
+describe('Container Component', () => {
+  test('Should render Container with props', () => {
+    makeSut()
+    const container = screen.getByTestId('container')
+    expect(container.style.maxWidth).toBe('500px')
+    expect(container.children.length).toBe(1)
+  });
+});
