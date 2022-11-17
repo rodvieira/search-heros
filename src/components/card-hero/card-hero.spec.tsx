74b881a7ccbdd0a281b9b99a2ba1316697c10d64
Rodrigo Vieira <rodrigodenizads@gmail.com>
2022-11-16 22:44:01 -0300

    test: implemented test in onClick function in cardHero component

--- src/components/card-hero/card-hero.spec.tsx
+++ src/components/card-hero/card-hero.spec.tsx
@@ -1,6 +1,7 @@
-import { CardHero } from '@/components'
 import React from 'react'
-import { render, screen } from '@testing-library/react'
+import { render, screen, fireEvent } from '@testing-library/react'
+
+import { CardHero } from '@/components'
 import { Character } from '@/protocols/character'
 
 const characterMock: Character = {
@@ -16,17 +17,17 @@ const characterMock: Character = {
   favorite: false
 }
 
-const makeSut = () => {
+const makeSut = (onClickFn = jest.fn(), favoriteEventFn = jest.fn()) => {
   return render(
     <CardHero
       character={characterMock}
-      onClick={jest.fn()}
-      favoriteEvent={jest.fn()}
+      onClick={onClickFn}
+      favoriteEvent={favoriteEventFn}
     />
   )
 }
 
-describe('InputSearch Component', () => {
+describe('CardHero Component', () => {
   test('Should render CardHero', () => {
     makeSut()
     const heroName = screen.getByTestId('character-name')
@@ -34,4 +35,12 @@ describe('InputSearch Component', () => {
     expect(heroName.textContent).toBe(characterMock.name)
     expect(heroImage).toHaveAttribute('src', `${characterMock.thumbnail.path}.${characterMock.thumbnail.extension}`)
   });
+
+  test('Should call onClick function when click in link', () => {
+    const onClickFn = jest.fn()
+    makeSut(onClickFn)
+    const characterLink = screen.getByTestId('character-link')
+    fireEvent.click(characterLink)
+    expect(onClickFn).toHaveBeenCalledTimes(1)
+  })
 });
