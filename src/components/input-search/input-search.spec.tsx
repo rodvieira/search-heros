d8f524fd83f61550844d63ed9b2f927019d39fa3
Rodrigo Vieira <rodrigodenizads@gmail.com>
2022-11-17 21:58:07 -0300

    test: implements userEvent in inputSearch tests

--- src/components/input-search/input-search.spec.tsx
+++ src/components/input-search/input-search.spec.tsx
@@ -1,22 +1,47 @@
 import InputSearch from './input-search'
 import React from 'react'
-import { render } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+
+
+const sendQueryMock = jest.fn()
 
 const makeSut = (custom: boolean = false) => {
-  return render(<InputSearch custom={custom} />)
+  return render(<InputSearch custom={custom} sendQuery={sendQueryMock} />)
 }
 
 describe('InputSearch Component', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
   test('Should render InputSearch', () => {
-    const inputSearch = makeSut()
-    const input = inputSearch.getByTestId('input-search') as HTMLInputElement
+    makeSut()
+    const input = screen.getByTestId('input-search') as HTMLInputElement
     expect(input.placeholder).toBe('Procure por heróis')
     expect(input.className).not.toContain('custom')
   });
 
   test('Should render InputSearch with custom', () => {
-    const inputSearch = makeSut(true)
-    const input = inputSearch.getByTestId('input-search') as HTMLInputElement
+    makeSut(true)
+    const input = screen.getByTestId('input-search') as HTMLInputElement
     expect(input.className).toContain('custom')
+  });
+
+  test('Should call handleKeyDown function when press enter key', () => {
+    makeSut()
+    const input = screen.getByTestId('input-search') as HTMLInputElement
+    fireEvent.focus(input)
+    fireEvent.keyDown(input, { key: 'Enter', code: 'Enter', charCode: 13 })
+    expect(sendQueryMock).toHaveBeenCalledTimes(1)
+  });
+
+  test('Should call handleKeyDown function with correct value', async () => {
+    makeSut()
+    const inputText = 'some text here'
+    const input = screen.getByTestId('input-search') as HTMLInputElement
+    await userEvent.type(input, inputText)
+    fireEvent.keyDown(input, { key: 'Enter', code: 'Enter', charCode: 13 })
+    expect(sendQueryMock).toHaveBeenCalledWith(inputText)
   });
 });
