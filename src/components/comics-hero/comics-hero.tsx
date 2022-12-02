a6b5fb6dbedfedbd734c9019546107db56614c60
Rodrigo Vieira <rodrigodenizads@gmail.com>
2022-12-01 23:22:16 -0300

    feat: add attributes for unit tests in component comicsHero

--- src/components/comics-hero/comics-hero.tsx
+++ src/components/comics-hero/comics-hero.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { Comics } from '@/protocols/comics'
+import { Comics } from '@/types/comics'
 import Styles from './comics-hero-styles.scss'
 
 type Props = {
@@ -12,12 +12,16 @@ const ComicsHero: React.FC<Props> = ({ characterComics }: Props) => {
     <div className={Styles.contentCardComics}>
       <h2>Últimos lançamentos:</h2>
       {characterComics.map((item) => (
-        <div className={Styles.cardComics} key={item.title}>
+        <div
+          className={Styles.cardComics}
+          key={item.title}
+          data-testid="comics-wrap"
+        >
           <img
             src={`${item.thumbnail?.path}.${item.thumbnail?.extension}`}
             alt={item.title}
           />
-          <span>{item?.title}</span>
+          <span data-testid="comics-title">{item?.title}</span>
         </div>
       ))}
     </div>
