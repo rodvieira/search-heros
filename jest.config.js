9847c624c6b1d00546375440ffb7afbdbcd77a6f
Rodrigo Vieira <rodrigodenizads@gmail.com>
2023-07-18 21:15:12 -0300

    style: format jest.config file

--- jest.config.js
+++ jest.config.js
@@ -1,26 +1,22 @@
 module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: [
-    '<rootDir>/src/**/*.{ts,tsx}', '!**/*.d.ts',
+    '<rootDir>/src/**/*.{ts,tsx}',
+    '!**/*.d.ts',
     '!<rootDir>/src/main/**/*',
     '!<rootDir>/src/**/index.ts',
-    '!**/*.d.ts'
+    '!**/*.d.ts',
   ],
-  setupFilesAfterEnv: [
-    "<rootDir>/src/main/config/setupTests.ts"
- ],
+  setupFilesAfterEnv: ['<rootDir>/src/main/config/setupTests.ts'],
   coverageDirectory: 'coverage',
-  testPathIgnorePatterns: [
-    '<rootDir>/node_modules/',
-    '<rootDir>/cypress/'
-  ],
+  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/cypress/'],
   testEnvironment: 'jsdom',
   transform: {
     '.+\\.(ts|tsx)$': 'ts-jest',
-    '^.+\\.svg$': 'jest-transform-stub'
+    '^.+\\.svg$': 'jest-transform-stub',
   },
   moduleNameMapper: {
     '@/(.*)': '<rootDir>/src/$1',
-    '\\.scss$': 'identity-obj-proxy'
-  }
+    '\\.scss$': 'identity-obj-proxy',
+  },
 }
