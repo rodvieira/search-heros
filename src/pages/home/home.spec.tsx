c7bd2f81c64be2c5eb5825ea3142adbc40379fe8
Rodrigo Vieira <rodrigodenizads@gmail.com>
2023-07-23 16:57:05 -0300

    test: add test just list favorites when click in just favorites

--- src/pages/home/home.spec.tsx
+++ src/pages/home/home.spec.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { act, fireEvent, render, screen, waitFor } from '@testing-library/react'
+import { act, fireEvent, render, screen } from '@testing-library/react'
 
 import { Home } from '@/pages'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
@@ -7,20 +7,22 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { listCharactersMock } from '@/test/mock-adapter'
 import { FavoriteContext } from '@/contexts/favorite-context'
 import * as useFetchCharacters from '@/hooks/useFetchCharacters'
+
 import userEvent from '@testing-library/user-event'
 
 const fetchCharactersMock = jest.fn()
-const handleOnlyFavoritesMock = jest.fn()
+const handleSetFavoriteCharacterMock = jest.fn()
 
 const useFetchCharactersSpy = jest.spyOn(
   useFetchCharacters,
   'useFetchCharacters'
 )
+
 const makeSut = () => {
   return render(
     <FavoriteContext.Provider
       value={{
-        favorite: ['10'],
+        favorites: [{ ...listCharactersMock[0] }],
         handleSetFavorite: jest.fn(),
         handleRemoveFavorite: jest.fn(),
       }}
@@ -44,7 +46,7 @@ describe('Home Page', () => {
       loading: true,
       characters: [],
       fetchCharacters: fetchCharactersMock,
-      handleOnlyFavorites: handleOnlyFavoritesMock,
+      handleSetFavoriteCharacter: handleSetFavoriteCharacterMock,
     })
     makeSut()
     const loading = screen.getByTestId('loading')
@@ -56,7 +58,7 @@ describe('Home Page', () => {
       loading: false,
       characters: listCharactersMock,
       fetchCharacters: fetchCharactersMock,
-      handleOnlyFavorites: handleOnlyFavoritesMock,
+      handleSetFavoriteCharacter: handleSetFavoriteCharacterMock,
     })
     makeSut()
     const amountCharactersListed = screen.getByText('Encontrado 2 heróis')
@@ -71,7 +73,7 @@ describe('Home Page', () => {
       loading: false,
       characters: [],
       fetchCharacters: fetchCharactersMock,
-      handleOnlyFavorites: handleOnlyFavoritesMock,
+      handleSetFavoriteCharacter: handleSetFavoriteCharacterMock,
     })
     makeSut()
     const input = screen.getByTestId('input-search')
@@ -89,7 +91,7 @@ describe('Home Page', () => {
       loading: false,
       characters: [],
       fetchCharacters: fetchCharactersMock,
-      handleOnlyFavorites: handleOnlyFavoritesMock,
+      handleSetFavoriteCharacter: handleSetFavoriteCharacterMock,
     })
     makeSut()
     const toggle = screen.getByTestId('toggle')
@@ -98,5 +100,15 @@ describe('Home Page', () => {
     })
     expect(fetchCharactersMock).toHaveBeenCalled()
     expect(fetchCharactersMock).toHaveBeenCalledWith({ orderBy: 'name' })
+  })
+
+  test('Should show just favorites when click in favorites', async () => {
+    makeSut()
+    const justFavorites = screen.getByTestId('just-favorites')
+    await act(() => {
+      fireEvent.click(justFavorites)
+    })
+    expect(screen.queryByText('A Bomb')).toBeVisible()
+    expect(screen.queryByText('AIM')).toBeNull()
   })
 })
